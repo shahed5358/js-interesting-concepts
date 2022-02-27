019b56fbdc7ee311e8f09d73e87552d8e8345356
shahed5358e <mohammadshahed5358@gmail.com>
2022-02-27 14:25:08 +0600

    call back more

--- callback.js
+++ callback.js
@@ -1,6 +1,23 @@
-function welcomeMessage() {
-    console.log(myObj);
+function welcomeMessage(name, greetHandler) {
+    greetHandler(name);
 }
 // const name = ['tom hanks', 'tom brady', 'tom cruise'];
-const myObj = { name: 'tom', age: 15 }
-welcomeMessage(myObj);
+// const myObj = { name: 'tom', age: 15 }
+function greetMorning(name) {
+    console.log('good morning', name)
+}
+function greetAfternoon(name) {
+    console.log('good afternoon', name)
+}
+welcomeMessage('tom hanks', greetMorning);
+welcomeMessage('sakib hanks', greetMorning);
+welcomeMessage('shahed', greetAfternoon)
+
+function handleClick() {
+    console.log('button is clicked')
+}
+document.getElementById('my-btn').addEventListener('click',
+    handleClick)
+document.getElementById('my-btn').addEventListener('click', function () {
+    console.log('btn is clicked')
+})
