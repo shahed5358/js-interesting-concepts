7fa62b003d007530950224b59bd63ba0dc6232cb
shahed5358e <mohammadshahed5358@gmail.com>
2022-02-27 14:39:57 +0600

    summary add

---
+++ summary.js
@@ -0,0 +1,31 @@
+const a = 'alia ';
+const b = 23;
+const c = true;
+// null 
+// undefined
+
+// primitive
+// object
+
+const nums = [2, 2, 3, 4, 5];
+// console.log(typeof nums);
+
+// array.isArray(nums);
+
+function triple(x, y, z) {
+    // x = 111;
+    // y = 222;
+    // z = 333;
+    x.age = 45;
+}
+// console.log(typeof triple)
+// console.log(triple.length);
+const num1 = 3;
+const num2 = 4;
+const num3 = 7;
+// triple(num1, num2, num3)
+const myObj = { name: 'koddos', age: 12 }
+triple(myObj);
+console.log(myObj);
+// console.log(typeof null);
+// console.log(num1, num2, num3)
