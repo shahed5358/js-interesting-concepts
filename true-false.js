32df2802b4ad7665022ac132a94bf6f93852c234
shahed5358e <mohammadshahed5358@gmail.com>
2022-02-27 11:47:39 +0600

    true or false js added

---
+++ true-false.js
@@ -0,0 +1,24 @@
+/*
+falsy:
+false
+0 
+empty string 
+undefined
+null
+NaN
+---------------------------------
+truthy:
+true
+any number (positive or negative)
+any string including single whitespace, '0', 'false'
+[], {}
+anything else that is not falsy will be truthy
+*/
+let x = {};
+console.log('value of x', x)
+if (x) {
+    console.log('variable is truthy');
+}
+else {
+    console.log('variable is falsy');
+}
